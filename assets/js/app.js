5bff72a03c359d9238a8537bb25f4418bfd5f0b8
KelvinKSP <kelvinpenha10@gmail.com>
2023-02-15 13:38:46 -0300

    First Commit

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const scrollUp = document.querySelector("#scroll-up");
+const checkbox = document.querySelector("#checkbox");
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("ul");
+const navLink = document.querySelectorAll("#nav-link");
+
+
+const openMenu = () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const closeMenu = () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+  });
+
+
+hamburger.addEventListener("click", openMenu);
